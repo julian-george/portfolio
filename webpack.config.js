ca3846d54f9e8a4898759261fe89700481c05016
FudgeDaMuffin <juliancgeorge@gmail.com>
2021-05-19 11:26:26 -0400

    Compiling JS with webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry:'./src/index.js',
+    mode:'development',
+    output: {
+        path:path.resolve(__dirname,'static/js'),
+        filename:'compiled.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                    presets: ['@babel/preset-env']
+                    }
+                }
+            },
+      ]
+    }
+}
